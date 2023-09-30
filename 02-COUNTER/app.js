c8209a9546e75d4ebc72113e7168f956bcf9b50e
unknown <olagoketunde600@gmail.com>
2023-09-30 05:17:30 -0500

    decrease and reset bottom working

--- 02-COUNTER/app.js
+++ 02-COUNTER/app.js
@@ -3,15 +3,22 @@ const decrEl = document.querySelector('.decrease');
 const resetEl = document.querySelector('.reset');
 const incrEl = document.querySelector('.increase');
 
+let count = 0;
 
 decrEl.addEventListener('click', function(){
-    let count = 0;
-    let i = count - 1
-    console.log(i);
-    numEl.textContent = i;
+     count--
+    console.log(count);
+    numEl.textContent = count;
 
 })
 
 resetEl.addEventListener('click', function(){
-    numEl.textContent = 0;
+    count = 0
+    numEl.textContent = count;
+})
+
+incrEl.addEventListener('click', function(){
+    count++
+    console.log(count);
+    numEl.textContent = count
 })
