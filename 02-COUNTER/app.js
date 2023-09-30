2ef1fdec9c82665718f055a16d10f6d46fc77976
unknown <olagoketunde600@gmail.com>
2023-09-30 04:01:49 -0500

    personal JS  attempt without watcing the video

---
+++ 02-COUNTER/app.js
@@ -0,0 +1,17 @@
+const numEl = document.querySelector(".value");
+const decrEl = document.querySelector('.decrease');
+const resetEl = document.querySelector('.reset');
+const incrEl = document.querySelector('.increase');
+
+
+decrEl.addEventListener('click', function(){
+    let count = 0;
+    let i = count - 1
+    console.log(i);
+    numEl.textContent = i;
+
+})
+
+resetEl.addEventListener('click', function(){
+    numEl.textContent = count;
+})
