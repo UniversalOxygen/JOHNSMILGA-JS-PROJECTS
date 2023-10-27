ef6aead2b2d93e58e5ed0d079117ed60fc35d967
unknown <olagoketunde600@gmail.com>
2023-10-27 07:56:51 -0500

    filtering of menu

--- 08-MENU/app.js
+++ 08-MENU/app.js
@@ -75,9 +75,32 @@ const menu = [
 
 
 const sectionCenter = document.querySelector('.section-center');
+const filterBtns = document.querySelectorAll('.filter-btn')
+
 
  window.addEventListener("DOMContentLoaded", function(){
-  let displayMenu = menu.map(function (item) {
+  displayMenuItems(menu)
+ })
+
+filterBtns.forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    const category = e.currentTarget.dataset.id
+    const menuCategory = menu.filter(function(menuItem) {
+if (menuItem.category === category)
+      return menuItem
+    })
+    if (category ==="all"){
+      displayMenuItems(menu)
+    } else {
+      displayMenuItems(menuCategory)
+    }
+  })
+})
+
+
+
+ function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
     
     return ` <article class="menu-item">
               <img src=${item.img} class="photo" alt=${item.title}>
@@ -92,4 +115,4 @@ const sectionCenter = document.querySelector('.section-center');
   })
   displayMenu = displayMenu.join("")
   sectionCenter.innerHTML = displayMenu
- })
+ }
